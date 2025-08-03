c839df86f728b23592f5025cf5199c6e373db651
Emil Z <istarnightstudios@gmail.com>
2025-08-03 15:44:37 -0700

    Gitignore fix to see DI files

---
+++ Dependency Injection/Assets/Dependancy Injection System/Injector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace DependancyInjection
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method)]
+    public sealed class InjectorAttribute : Attribute
+    {
+        public InjectorAttribute() { }
+
+    }
+    [AttributeUsage(AttributeTargets.Method)]
+
+    public sealed class ProvideAttribute : Attribute
+    { 
+        public ProvideAttribute() { }
+    }
+
+    
+
+    public class ClassA : MonoBehaviour
+    {
+
+    }
+
+    public class ClassB : MonoBehaviour
+    {
+
+    }
+
+
+    public class Injector
+    {
+
+
+
+    }
+
+
+
+
+}
